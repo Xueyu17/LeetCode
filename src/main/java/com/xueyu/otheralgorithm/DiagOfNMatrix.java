04af52126614f518b882388c89ac9d8658854ac0
Xueyu Li <401756139@qq.com>
2016-09-27 11:46:11 +0800

    print diagonal elements of N*N matrix

--- src/main/java/com/xueyu/otheralgorithm/DiagOfNMatrix.java
+++ src/main/java/com/xueyu/otheralgorithm/DiagOfNMatrix.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 public class DiagOfNMatrix {
     @Test
     public void test(){
+        //输出：1, 2 6, 3 7 11, 4 8 12 16, 5 9 13 17 21, 10 14 18 22, 15 19 23,20 24, 25
         int[][] arr = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
         diagOfNMatrix(arr, 5);
     }
